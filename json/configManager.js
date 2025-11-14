78d1beb12c4639b94892456d5fa4442f5b1b81f8
RavaAchmad <ravabbok@gmail.com>
2025-11-14 13:00:44 +0700

    update dikit aja

--- json/configManager.js
+++ json/configManager.js
@@ -1,16 +1,18 @@
 
 import fs from 'fs/promises';
 import path from 'path';
-
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url); 
+const __dirname = path.dirname(__filename);
 // Ganti nama file-nya di sini
-const configPath = path.resolve("./json/brainiesDB.json");
+const configPath = path.resolve(__dirname, 'brainiesDB.json'); 
 
 async function readConfig() {
   try {
     const data = await fs.readFile(configPath, 'utf8');
     return JSON.parse(data);
   } catch (error) {
-    console.error('File DB ga ada, gue bikinin yang baru.', error);
+    console.error('File DB ga ada', error);
     return {};
   }
 }
