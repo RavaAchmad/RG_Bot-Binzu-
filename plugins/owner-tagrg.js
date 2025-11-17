e75f4197185ec751d6cee9c860a63d357ac3c873
RavaAchmad <ravabbok@gmail.com>
2025-11-17 13:30:05 +0700

    debug

--- plugins/owner-tagrg.js
+++ plugins/owner-tagrg.js
@@ -85,7 +85,7 @@ let handler = async (m, { conn, text, command }) => {
     messageText += `KHUSUS untuk jadwal pembelajaran SNBT akan share di grup ini ya, jadi kalau ada temennya yang belum masuk grup ini harap colek colek yaa temen-temen 😊\n\n`;
     messageText += `Jadwal hari ini\n`;
     messageText += `Sesi 1  (17.00 - 20.30)\n- SNBT\n\n`;
-    messageText += `Sesi 2  (19.00 - 20.30)\n- SNBT\n\n`;
+    messageText += `Sesi 2  (19.00 - 20.30)\n- SNBTv @${groupId}\n\n`;
     messageText += `Info kelasnya sudah Kak Indri share kemarin di atas bisa di-scroll aja ya, atau bisa cek di aplikasi. Jika jadwal belum berubah, masih tahap penyesuaian jadwal kelas terbaru ya. Terima kasih 😊\n\n`;
 
     // METODE 1: Display custom alias dengan hidden mention
