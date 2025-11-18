29ed8e6b063e94910b4ffb5149c506e9e1d4f28b
RavaAchmad <ravabbok@gmail.com>
2025-11-18 10:55:26 +0700

    update del murid

--- plugins/owner-delmurid.js
+++ plugins/owner-delmurid.js
@@ -1,7 +1,7 @@
 // delmurid.js
 import { readConfig, writeConfig } from '../json/configManager.js';
 
-const MODS = ['6281212035575@s.whatsapp.net'];
+const MODS = ['6281212035575@s.whatsapp.net', '217333968683183@lid'];
 
 let handler = async (m, { conn, text }) => {
   if (!MODS.includes(m.sender)) {
